da28d98cdecf4d7193ae0c94d4f1d19541adab16
up1512001 <utsav.patel1512@outlook.com>
2022-08-29 15:05:41 +0530

    router updated

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-const Auth = require("./routes/nQueen")
+
+
+
+const Auth = require("./routes/authRoute")
 
 app.use("/api",Auth)
 
 app.use(bodyParser.json());
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 9000
 
-app.listen(port , ()=>{
-    console.log(`listining on port ${port}`);
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`)
 })
